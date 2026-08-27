49eae0263ba60a61bc2a10e95502acbd20b93085
benkhalife <nvioir@gmail.com>
2026-08-27 07:21:46 -0300

    build: update dist with auto-update publish logic and scene-level BGM

--- assets/BufferResource-Bz2zv1Uk.js
+++ assets/BufferResource-Bz2zv1Uk.js
@@ -1,4 +1,4 @@
-import{w as _,a as g,S as c,u as U,B as y,b as l,E as I,c as m}from"./index-DAx-d9n7.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,a as g,S as c,u as U,B as y,b as l,E as I,c as m}from"./index-JcOGM82w.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
